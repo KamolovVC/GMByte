32d577727730073324d418272be961de750593c9
KamolovVC <38563959+KamolovVC@users.noreply.github.com>
2018-04-20 19:00:27 +0300

    Create task2.java

---
+++ lesson2/task2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Ура! Сделан мой первый шаг в java! ");
+        System.out.print("Ура! Сделан мой первый шаг в java! ");
+        System.out.print("Ура! Сделан мой первый шаг в java! ");
+        System.out.print("Ура! Сделан мой первый шаг в java! ");
+        System.out.print("Ура! Сделан мой первый шаг в java! ");
+
+    }
+}
