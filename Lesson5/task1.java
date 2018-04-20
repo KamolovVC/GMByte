9027cafd409906033741b144492be68501e99a59
KamolovVC <38563959+KamolovVC@users.noreply.github.com>
2018-04-21 00:02:59 +0300

    Create task1.java

---
+++ Lesson5/task1.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        vasya();
+        petya();
+
+    }
+    static void vasya () {
+        System.out.println("Я vasya. Звали?");
+    }
+    static void petya () {
+        System.out.println("Я petya. Звали?");
+    }
+}
