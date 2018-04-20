9ea2aacb42b1e8813cc5faa56dadcc88bb522c60
KamolovVC <38563959+KamolovVC@users.noreply.github.com>
2018-04-21 01:28:07 +0300

    Create task1.java

---
+++ Lesson7/task1.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        byte variable_1 = 5;
+        method(variable_1);
+
+
+    }
+    static void method (byte a) {
+        System.out.println("В переменной variable_1 хранится число: " + a);
+
+    }
+}
