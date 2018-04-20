13d76e01a7aaabbc21311fd96f041da571358aa3
KamolovVC <38563959+KamolovVC@users.noreply.github.com>
2018-04-20 18:54:39 +0300

    Create task1.java

---
+++ lesson2/task1.java
@@ -0,0 +1,13 @@
+package Lesson1;
+
+public class task1 {
+    public static void main(String[] args) {
+
+        System.out.println("Ура! Я начинаю учить Java");
+        System.out.println("Ура! Я начинаю учить Java");
+        System.out.println("Ура! Я начинаю учить Java");
+        System.out.println("Ура! Я начинаю учить Java");
+        System.out.println("Ура! Я начинаю учить Java");
+
+    }
+}
