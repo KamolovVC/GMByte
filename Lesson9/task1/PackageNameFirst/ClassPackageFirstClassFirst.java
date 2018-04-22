614aecbfe8c2df74e78ce4a496f521f4f75050ae
KamolovVC <38563959+KamolovVC@users.noreply.github.com>
2018-04-23 00:54:29 +0300

    Create ClassPackageFirstClassFirst.java

---
+++ Lesson9/task1/PackageNameFirst/ClassPackageFirstClassFirst.java
@@ -0,0 +1,18 @@
+package PackageNameFirst;
+
+public class ClassPackageFirstClassFirst {
+    int variableDefault;
+    public int variablePublic = 5;
+    private int variablePrivate;
+
+    void methodDefault () {
+
+    }
+    public void methodPublic () {
+        System.out.println("Я публичный метод!");
+
+    }
+    private void methodPrivate () {
+
+    }
+}
