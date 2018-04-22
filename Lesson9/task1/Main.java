84315675c6462d92da427a9fa7ad0bd2ca0183f0
KamolovVC <38563959+KamolovVC@users.noreply.github.com>
2018-04-23 00:53:54 +0300

    Create Main.java

---
+++ Lesson9/task1/Main.java
@@ -0,0 +1,10 @@
+import PackageNameFirst.ClassPackageFirstClassFirst;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPackageFirstClassFirst classPackageFirstClassFirst = new ClassPackageFirstClassFirst();
+        System.out.println(classPackageFirstClassFirst.variablePublic);
+        classPackageFirstClassFirst.methodPublic();
+
+    }
+}
