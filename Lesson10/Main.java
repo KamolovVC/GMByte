20aa6c392c567feafce75b543b53e61735cfa3a8
KamolovVC <dmitzorov@yandex.ru>
2018-04-23 01:38:58 +0300

    Lesson10

---
+++ Lesson10/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        NotMain notMain = new NotMain(1,2,3,4);
+
+    }
+}
---
+++ Lesson10/NotMain.java
@@ -0,0 +1,6 @@
+public class NotMain {
+    public NotMain (int a, int b, int c, int d) {
+        System.out.println("Меня создали!");
+
+    }
+}
