b3b92416a116906c25f92134f6782ddf88b2cf7d
KamolovVC <dmitzorov@yandex.ru>
2018-04-25 22:14:35 +0300

    no message

---
+++ Lesson12/Task_1_2.java
@@ -0,0 +1,70 @@
+package Lesson12;
+
+public class Task_1_2 {
+    public static void main(String[] args) {
+        byte a = 1;
+        switch (a) {
+            case 1:
+                System.out.println("a = 1");
+                break;
+            case 2:
+                System.out.println("a = 2");
+                break;
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+            case 32:
+            case 33:
+            case 34:
+            case 35:
+            case 36:
+            case 37:
+            case 38:
+            case 39:
+            case 40:
+            case 41:
+            case 42:
+            case 43:
+            case 44:
+            case 45:
+            case 46:
+            case 47:
+            case 48:
+            case 49:
+                System.out.println("a < 50");
+                break;
+            case 50:
+                System.out.println("a = 50");
+                break;
+            default:
+                System.out.println("a > 50");
+                break;
+        }
+    }
+}
