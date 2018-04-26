614e894a521d5cb79e53875e5c0de6e9b5eb6716
KamolovVC <dmitzorov@yandex.ru>
2018-04-27 01:42:21 +0300

    Приведение типов

---
+++ Lesson17/Main.java
@@ -0,0 +1,20 @@
+package Lesson17;
+
+public class Main {
+    public static void main(String[] args) {
+        long a = 10;
+        byte b;
+        int c;
+        b = (byte) a;
+        c = (int) a;
+        int x = 20, y = 30;
+        byte k;
+        k = (byte) (x + y);
+        byte t = 5;
+        int w;
+        w = t;
+        byte t1 = 5;
+        int w1 = 5;
+        t1 = (byte) w1;
+    }
+}
