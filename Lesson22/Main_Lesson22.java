222062645a2421b8fadf1d8a5c3d2f58e2f3ca50
KamolovVC <dmitzorov@yandex.ru>
2018-05-12 00:17:45 +0300

    Strings

---
+++ Lesson22/Main_Lesson22.java
@@ -0,0 +1,9 @@
+package Lesson22;
+
+public class Main_Lesson22 {
+    public static void main(String[] args) {
+        String s = "Теперь я умею создавать и хранить строки. Я даже прикрепил к ней цифру: ";
+        byte a = 5;
+        System.out.println(s + a);
+    }
+}
